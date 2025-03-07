605a466385278f279638f5b85dbe162833e3cd80
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-07 11:47:50 +0000

    Added gameboard class and rendergameboard func

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+
+// generate gameboard 3 x 3
+class Gameboard {
+    constructor() {
+
+    }
+    gameBoard = [
+        ["-","-","-"],
+        ["-","-","-"],
+        ["-","-","-"]
+        ];
+}
+
+function renderGameBoard(gameBoardObject) {
+    const wrapper = document.getElementById("wrapper");
+
+    for(let i = 0; i < gameBoardObject.length; i++) {
+        const outIndex = [i];
+        for(let j = 0; j < gameBoardObject[i].length; j++) {
+            const div = document.createElement("button");
+            div.setAttribute("data-id", `${outIndex}${j}`)
+            wrapper.append(div);
+        };
+    };
+};
+
+const gameBoard = new Gameboard();
+renderGameBoard(gameBoard.gameBoard);
