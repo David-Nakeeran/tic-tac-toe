c08c3aabf1b03eadfa73993753455b2830432391
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-07 17:01:10 +0000

    refactored

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("Hello World")
+class Player {
+    constructor(playerIcon) {
+        this.playerIcon = playerIcon;
+    };
+};
 
 class Gameboard {
     constructor() {
@@ -11,63 +15,120 @@ class Gameboard {
         ];
 };
 
-class Player {
-    constructor(playerIcon) {
-        this.playerIcon = playerIcon;
-    }
-}
-
 class GameController {
     constructor(gameBoard) {
-        this.board = gameBoard;
+        this.board = gameBoard.board;
+        this.isPlayer1Active = true;
     }
 }
 
-function renderGameBoard(gameBoardObject) {
-    const wrapper = document.getElementById("wrapper");
+class Display {
+    constructor(gameController) {
+        this.gameBoard = gameController.board;
+        this.isPlayer1Active = gameController.isPlayer1Active;
+    }
 
-    for(let i = 0; i < gameBoardObject.length; i++) {
-        const outIndex = [i];
-        for(let j = 0; j < gameBoardObject[i].length; j++) {
-            const div = document.createElement("button");
-            div.setAttribute("data-id", `${outIndex}${j}`);
-            wrapper.append(div);
+    renderGameBoard() {
+        const wrapper = document.getElementById("wrapper");
+    
+        for(let i = 0; i < this.gameBoard.length; i++) {
+            const outIndex = [i];
+            for(let j = 0; j < this.gameBoard[i].length; j++) {
+                const div = document.createElement("button");
+                div.setAttribute("data-id", `${outIndex}${j}`);
+                wrapper.append(div);
+            };
         };
+    };
+
+    assignIconToBoard() {
+        
+        const cells = document.querySelectorAll("[data-id]");
+        
+            cells.forEach(element => {
+                console.log(this.gameBoard);
+                element.addEventListener("click", (e) => {
+                    const cellId = e.target.dataset.id;
+                    
+                    const outerIndex = cellId.slice(0,1);
+                    const innerIndex = cellId.slice(1,2);
+
+                    if(!this.isPlayer1Active) {
+                        element.textContent = this.gameBoard[outerIndex][innerIndex] = "O";
+                        this.isPlayer1Active = !this.isPlayer1Active;
+                        return;
+                    } else {
+                        element.textContent = this.gameBoard[outerIndex][innerIndex] = "X";
+                        this.isPlayer1Active = !this.isPlayer1Active;
+                    }
+                    
+                });
+            });
     };
 };
 
-function handleClicks(gameBoardObject) {
+class App {
+    constructor() {
+        this.player1 = new Player("X");
+        this.player2 = new Player("O");
+        this.gameBoard = new Gameboard();
+        this.gameController = new GameController(this.gameBoard);
+        this.display = new Display(this.gameController);
+    };
+
+    start() {
+        this.display.renderGameBoard();
+        this.display.assignIconToBoard();
+    }
+};
+
+// function handleClicks(gameBoardObject) {
+//     const cells = document.querySelectorAll("[data-id]");
+//     let cellId;
+
+//     cells.forEach(element => {
+//         element.addEventListener("click", function(e){
+//             cellId = e.target.dataset.id;
+
+            // const outerIndex = cellId.slice(0,1);
+            // const innerIndex = cellId.slice(1,2);
+
+            // element.textContent = gameBoardObject[outerIndex][innerIndex] = "X";
+//         });
+//     });
+//     return cellId;
+// }
+
+function assignIconToBoard(gameBoardObject) {
+
     const cells = document.querySelectorAll("[data-id]");
-    let cellId;
-    cells.forEach(element => {
-        element.addEventListener("click", function(e){
-            cellId = e.target.dataset.id;
+        
+    let isPlayer1Active = true;
+        cells.forEach(element => {
+            element.addEventListener("click", function(e){
+                const cellId = e.target.dataset.id;
+    
+                const outerIndex = cellId.slice(0,1);
+                const innerIndex = cellId.slice(1,2);
 
-            const outerIndex = cellId.slice(0,1);
-            const innerIndex = cellId.slice(1,2);
-
-            element.textContent = gameBoardObject[outerIndex][innerIndex] = "X";
-
-            console.log(gameBoardObject);
-
+                if(!isPlayer1Active) {
+                    element.textContent = gameBoardObject[outerIndex][innerIndex] = "O";
+                    isPlayer1Active = !isPlayer1Active;
+                    return;
+                } else {
+                    element.textContent = gameBoardObject[outerIndex][innerIndex] = "X";
+                    isPlayer1Active = !isPlayer1Active;
+                }
+                
+            });
         });
-    });
-    return cellId;
 }
 
-function assignIconToBoard(gameBoardObject, cellId) {
-    // let outerIndex = cellId.slice(1, 2);
-    // console.log(outerIndex)
-    console.log(cellId)
-}
+const app = new App();
+app.start();
+
+// renderGameBoard(gameBoard.board);
 
 
-const player1 = new Player("X");
-const player2 = new Player("O");
 
-const gameBoard = new Gameboard();
-renderGameBoard(gameBoard.board);
-const gameController = new GameController(gameBoard.board);
-// assignIconToBoard(gameController.board, handleClicks());
-
-handleClicks(gameController.board);
+// assignIconToBoard(gameController.board);
