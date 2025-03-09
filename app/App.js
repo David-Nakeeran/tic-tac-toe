d407b76867e106c3f40defcdeb68e4f2f30d66b9
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-09 16:26:55 +0000

    Added App class

---
+++ app/App.js
@@ -0,0 +1,21 @@
+import Player from "../logic/Player.js";
+import Gameboard from "../logic/Gameboard.js";
+import GameController from "../logic/GameController.js";
+import Display from "../views/Display.js";
+
+export default class App {
+  constructor() {
+    this.player1 = new Player("X");
+    this.player2 = new Player("O");
+    this.gameBoard = new Gameboard();
+    this.gameController = new GameController(this.gameBoard);
+    this.display = new Display(this.gameController, this.gameBoard);
+  }
+
+  start() {
+    this.display.renderGameBoard();
+    this.display.playerAnnouncementPara();
+    this.display.announcePlayerTurn();
+    this.display.assignIconToBoard();
+  }
+}
