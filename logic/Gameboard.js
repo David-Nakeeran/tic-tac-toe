6dff0527b1dd7c64872249861015e9c640576a21
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-10 15:51:21 +0000

    formatting

--- logic/Gameboard.js
+++ logic/Gameboard.js
@@ -10,6 +10,14 @@ export default class Gameboard {
     return this.board[outerIndex][innerIndex] === "";
   }
 
+  getCellElement(outerIndex, innerIndex) {
+    if (this.board[outerIndex][innerIndex] === "X") {
+      return "X";
+    } else {
+      return "O";
+    }
+  }
+
   updateBoard(outerIndex, innerIndex, symbol) {
     this.board[outerIndex][innerIndex] = symbol;
   }
