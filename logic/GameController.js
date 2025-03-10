38e76513b4a6ba8e9b837f9771b96f0b519787ed
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-10 15:52:02 +0000

    winConditions method not working correctly

--- logic/GameController.js
+++ logic/GameController.js
@@ -24,4 +24,28 @@ export default class GameController {
 
     return null;
   }
+
+  winConditions() {
+    for (let i = 0; i < this.gameBoard.board.length; i++) {
+      console.log(this.gameBoard.board[i][0]);
+      console.log(this.gameBoard.board[i][2]);
+      let rowWin = this.gameBoard.board[i].every(
+        (cell) => cell === this.gameBoard.board[i][2]
+      );
+      if (rowWin) {
+        console.log("win");
+        console.log(this.gameBoard.board[i][0]);
+        break;
+      }
+    }
+  }
+  //   for (let j = 0; j < this.gameBoard.board[i].length; j++) {}
+  // }
+  // if (
+  //   this.gameBoard.board[0][1] === "X" &&
+  //   this.gameBoard.board[0][1] === "X" &&
+  //   this.gameBoard.board[0][2] === "X"
+  // ) {
+  //   console.log("Player 1 wins");
+  // }
 }
