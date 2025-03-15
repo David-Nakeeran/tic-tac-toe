605ab88a42ef6691f9f4a3f9399984c245d691eb
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-15 17:03:59 +0000

    refactored

--- logic/GameController.js
+++ logic/GameController.js
@@ -26,60 +26,71 @@ export default class GameController {
   }
 
   winConditions() {
-    // rows
-    const topRow = [...this.gameBoard.board[0]];
-    const middleRow = [...this.gameBoard.board[1]];
-    const bottomRow = [...this.gameBoard.board[2]];
+    const winArr = [];
+    for (let i = 0; i < this.gameBoard.board.length; i++) {
+      winArr.push([...this.gameBoard.board[i]]); // rows
+      winArr.push(this.gameBoard.board.map((row) => row[i])); //columns
+    }
 
-    // columns
-    const firstColumn = [
-      this.gameBoard.board[0][0],
-      this.gameBoard.board[1][0],
-      this.gameBoard.board[2][0],
-    ];
-    const secondColumn = [
-      this.gameBoard.board[0][1],
-      this.gameBoard.board[1][1],
-      this.gameBoard.board[2][1],
-    ];
-    const thirdColumn = [
-      this.gameBoard.board[0][2],
-      this.gameBoard.board[1][2],
-      this.gameBoard.board[2][2],
-    ];
+    winArr.push(this.gameBoard.board.map((row, index) => row[index])); // Left to right diagonal
+    winArr.push(
+      this.gameBoard.board.map((row, index) => row[row.length - 1 - index]) // right to left diagonal
+    );
 
-    // diagonals
-    const leftDiagonal = [
-      this.gameBoard.board[0][0],
-      this.gameBoard.board[1][1],
-      this.gameBoard.board[2][2],
-    ];
-    const rightDiagonal = [
-      this.gameBoard.board[0][2],
-      this.gameBoard.board[1][1],
-      this.gameBoard.board[2][0],
-    ];
-
-    const winArray = [
-      topRow,
-      middleRow,
-      bottomRow,
-      firstColumn,
-      secondColumn,
-      thirdColumn,
-      leftDiagonal,
-      rightDiagonal,
-    ];
-
-    for (let i = 0; i < winArray.length; i++) {
-      if (this.checkArrayForWin(winArray[i])) {
+    for (let i = 0; i < winArr.length; i++) {
+      if (this.checkArrayForWin(winArr[i])) {
         this.isWinConditionMet = true;
         break;
       }
     }
   }
-
   checkArrayForWin(arr) {
     return arr.every((cell) => cell === arr[0] && cell != "");
   }
 }
+// rows
+// const topRow = [...this.gameBoard.board[0]];
+// const middleRow = [...this.gameBoard.board[1]];
+// const bottomRow = [...this.gameBoard.board[2]];
+
+// columns
+// const firstColumn = [
+//   this.gameBoard.board[0][0],
+//   this.gameBoard.board[1][0],
+//   this.gameBoard.board[2][0],
+// ];
+// const secondColumn = [
+//   this.gameBoard.board[0][1],
+//   this.gameBoard.board[1][1],
+//   this.gameBoard.board[2][1],
+// ];
+// const thirdColumn = [
+//   this.gameBoard.board[0][2],
+//   this.gameBoard.board[1][2],
+//   this.gameBoard.board[2][2],
+// ];
+
+// diagonals
+// const leftDiagonal = [
+//   this.gameBoard.board[0][0],
+//   this.gameBoard.board[1][1],
+//   this.gameBoard.board[2][2],
+// ];
+// const rightDiagonal = [
+//   this.gameBoard.board[0][2],
+//   this.gameBoard.board[1][1],
+//   this.gameBoard.board[2][0],
+// ];
+
+//   const winArray = [
+//     topRow,
+//     middleRow,
+//     bottomRow,
+//     firstColumn,
+//     secondColumn,
+//     thirdColumn,
+//     leftDiagonal,
+//     rightDiagonal,
+//   ];
+//   }
+// }
