e3f1099f940148fbb9d5abe3737e1d155d117eb7
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-17 12:12:41 +0000

    Added resetGameboard method

--- logic/Gameboard.js
+++ logic/Gameboard.js
@@ -21,4 +21,8 @@ export default class Gameboard {
   updateBoard(outerIndex, innerIndex, symbol) {
     this.board[outerIndex][innerIndex] = symbol;
   }
+
+  resetGameboard(outerIndex, innerIndex) {
+    this.board[outerIndex][innerIndex] = "";
+  }
 }
